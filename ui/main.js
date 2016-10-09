43847ebd9f534e2a6f1360482d3a3449d28a415a
Krishna Mishra <krishnakumarmishra786@gmail.com>
2016-10-09 11:19:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,11 @@ element.innerHTML = 'New value';
 
 //move image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight(){
+ marginLeft=marginLeft+10;
+ img.style.marginLeft= marginLeft +'px';
+}
 img.onclick = function() {
-    img.style.marginLeft = '100px';
+    var interval = setinterval(moveRight, 100);
 };
