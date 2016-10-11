1770a652cc45ff9aad769b3f37d1d8a8db825cc6
Krishna Mishra <krishnakumarmishra786@gmail.com>
2016-10-12 03:24:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,8 +24,7 @@ button.onclick =function(){
 };
 
 //submit name
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
      // create a request object
@@ -49,7 +48,8 @@ submit.onclick=function(){
         //Not done yet
     };
     //make the request
-    
+    var nameInput=document.getElementById('name');
+    var name=nameInput.value;
     request.open('GET','http://krish786.imad.hasura-app.io/submit-name?name='+ name,true);
     request.send(null);
     
