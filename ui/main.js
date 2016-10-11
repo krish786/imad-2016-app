8df310287936eaff100d22fcb4989aca3d38679e
Krishna Mishra <krishnakumarmishra786@gmail.com>
2016-10-12 03:17:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,13 +28,29 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-  //make a request to the server and send the name 
-  //capture the list of names and render it as a list
-  var names=['name1' ,'name2', 'name3','name4'];
-  var list='';
-  for(var i=0;i<names.length;i++){
-      list += '<li>'+names[i] +'</li>';
-  }
-  var ul=document.getElementById('namelist');
-  ul.innerHTML=list;
+     // create a request object
+       var request = new XMLHttpRequest();
+    //capture the response and store in variable
+    request.onreadystatechange = function(){
+        if(request.readyState == XMLHttpRequest.DONE){
+            //take some action
+            if(request.status == 200){
+               //capture the list of names and render it as a list
+                 var names=request.responseText;
+                 names=JSON.parse(names);
+                 var list='';
+                  for(var i=0;i<names.length;i++){
+                    list += '<li>'+names[i] +'</li>';
+                   }
+                    var ul=document.getElementById('namelist');
+                   ul.innerHTML=list
+                   }
+        }
+        //Not done yet
+    };
+    //make the request
+    
+    request.open('GET','http://krish786.imad.hasura-app.io/submit-name?name='+ name,true);
+    request.send(null);
+    
 };
