43e908fa44f0665a34b14248c314b6f34934e22c
Krishna Mishra <krishnakumarmishra786@gmail.com>
2016-10-12 01:56:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,24 @@
 //counter code 
 var button =document.getElementById('counter');
-var counter = 0;
+
 button.onclick =function(){
     // make a request to thr counter endpoint
-
+       var request = new XMLHttpRequest();
     //capture the response and store in variable
+    request.onreadystatechange = function(){
+        if(request.readystate== XMLHttpReuest.DONE){
+            //take some action
+            if(request.status==200){
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+        //Not done yet
+    };
+    //make the request
     
-    //render the variable in the crrect span
-    counter =counter +1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
-}
+    request.open('GET','http://http://krish786.imad.hasura-app.io/counter',true);
+    reqest.send(null);
+    
+};
